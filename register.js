f93e4cd3fe15e82636175c2cb0a5659e97dc00c4
djunaedi <djunaedi.pakamundi@gmail.com>
2022-10-23 17:03:59 +0800

    tambahin href login

--- register.js
+++ register.js
@@ -1,76 +1,32 @@
-fetch('https://6353512ae822bad5278ac951.mockapi.io/users', {
-//   headers: {Authentication: 'Bearer Token'}
-})
-   .then(resp => resp.json())
-   .then(json => {
-    // console.log(json)
-
-    json.forEach(element => {
-        // console.log(element)
-        return arrayFetch(element)
-    }) 
-})
-
 let btn = document.getElementById("btn");
-
-
-//    validation
-function arrayFetch (b){
-    return {
-        b.name,
-        b.email,
-        b.password
-    }
-
-}
-
-console.log(wrap.name)
-
 
 btn.addEventListener('click', (e)=>{
     let username = document.getElementById('username').value;
     console.log(username)
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
-    e.preventDefault();
-    console.log(wrap.name)
-    if (username == "" ) {
-        window.alert("Please enter your username.");
-        username.focus();
-        return false;
-    }
+    // e.preventDefault();
+    // console.log(wrap.name)
 
-    if (username == wrap.name){
-        window.alert("username sudah ada.");
-        email.focus();
-        return false;
-    }
+    let data = {name : username, password : password,email : email}
 
-    if (email == "") {
-        window.alert("Please enter your email.");
-        email.focus();
-        return false;
-    }
-
-    if (email == wrap.email){
-        window.alert("email sudah digunakan.");
-        email.focus();
-        return false;
-    }
-
-    if (password == "") {
-        window.alert("Please enter your password.");
-        password.focus();
-        return false;
-    }
-
-    if (password == wrap.password){
-        window.alert("Password sudah digunakan.");
-        email.focus();
-        return false;
-    }
+    fetch('https://6353512ae822bad5278ac951.mockapi.io/users', {
+     method: 'POST',
+     body: JSON.stringify(data),
+     headers: {
+        'Content-Type': 'application/json'
+      },
+    })
+    .then(resp => resp.json())
+    .then(json => alert("registrasi sukses"))
+    .catch(err => alert("registrasi gagal"))
 
 })
+
+
+
+
+
 
 
 
